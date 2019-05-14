229bb59bbd7cd6b80891901ff04dc2b873b98b3b
Raul Fontenele <raulfontenele@edu.unifor.br>
2019-05-13 21:20:59 -0300

    Simulation and names Raul and Paulo

--- Code/write_Paulo.m
+++ Code/write_Paulo.m
@@ -1,7 +1,7 @@
 clc
 
 disp('========================');
-[t1,t2,t3] = cinematica_inversa4(100,0,-70)
+[t1,t2,t3] = cinematica_inversa(100,0,-70)
 [x,y,z] = direct_kinematic(t1,t2,t3)
 
 t11 = t1*180/pi
@@ -9,16 +9,7 @@ t22 = t2*180/pi
 t33 = t3*180/pi
 
 disp('========================');
-[t1,t2,t3] = cinematica_inversa4(60,0,-70);
-[x,y,z] = direct_kinematic(t1,t2,t3)
-
-t11 = t1*180/pi
-t22 = t2*180/pi
-t33 = t3*180/pi
-
-disp('========================');
-
-[t1,t2,t3] = cinematica_inversa4(60,30,-70);
+[t1,t2,t3] = cinematica_inversa(60,0,-70);
 [x,y,z] = direct_kinematic(t1,t2,t3)
 
 t11 = t1*180/pi
@@ -27,7 +18,7 @@ t33 = t3*180/pi
 
 disp('========================');
 
-[t1,t2,t3] = cinematica_inversa4(80,30,-70);
+[t1,t2,t3] = cinematica_inversa(60,30,-70);
 [x,y,z] = direct_kinematic(t1,t2,t3)
 
 t11 = t1*180/pi
@@ -36,7 +27,16 @@ t33 = t3*180/pi
 
 disp('========================');
 
-[t1,t2,t3] = cinematica_inversa4(80,0,-70);
+[t1,t2,t3] = cinematica_inversa(80,30,-70);
+[x,y,z] = direct_kinematic(t1,t2,t3)
+
+t11 = t1*180/pi
+t22 = t2*180/pi
+t33 = t3*180/pi
+
+disp('========================');
+
+[t1,t2,t3] = cinematica_inversa(80,0,-70);
 [x,y,z] = direct_kinematic(t1,t2,t3)
 
 t11 = t1*180/pi
